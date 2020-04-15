e40eb39adeb5bb7b4d1ed8373cac75dfc72cf3dd
zqb <734057028@qq.com>
2020-04-15 17:39:46 +0800

    删-

--- MvvmLib/src/main/java/com/zqb/mvvm/base/SimpleActivity.kt
+++ MvvmLib/src/main/java/com/zqb/mvvm/base/SimpleActivity.kt
@@ -24,19 +24,6 @@ import org.kodein.di.generic.instance
 
 abstract class SimpleActivity : InjectionActivity() {
 
-    private val mBroadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            when(intent.action) {
-                "android.intent.action.SCREEN_ON" -> {
-                    BarUtils.setNavBarVisibility(this@SimpleActivity, false)
-                }
-                "android.intent.action.SCREEN_OFF" -> {
-                    BarUtils.setNavBarVisibility(this@SimpleActivity, false)
-                }
-            }
-        }
-    }
-
     override val kodein: Kodein by retainedKodein {
         extend(parentKodein)
         bind<Activity>() with instance(this@SimpleActivity)
@@ -51,33 +38,20 @@ abstract class SimpleActivity : InjectionActivity() {
         BackgroundLibrary.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(layoutId)
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             getContainer()?.setPadding(
                 getContainer()?.paddingLeft ?: 0, (getContainer()?.paddingTop ?: 0) + BarUtils.getStatusBarHeight(),
                 getContainer()?.paddingRight ?: 0, getContainer()?.paddingBottom ?: 0
             )
-            BarUtils.setNavBarVisibility(this, false)
         }
-        ScreenUtils.toggleFullScreen(this)
         EventBus.getDefault().register(this)
-        val intentFilter = IntentFilter()
-        intentFilter.addAction("android.intent.action.SCREEN_ON")
-        intentFilter.addAction("android.intent.action.SCREEN_OFF")
-        registerReceiver(mBroadcastReceiver, intentFilter)
         initDataAndView()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        BarUtils.setNavBarVisibility(this@SimpleActivity, false)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         EventBus.getDefault().unregister(this)
-        unregisterReceiver(mBroadcastReceiver)
     }
 
     protected open fun onViewCreated() {
